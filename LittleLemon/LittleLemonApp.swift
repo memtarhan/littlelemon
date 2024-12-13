ddd3c6241932ef29aa448dcaa1d9c477178af85f
Mehmet Tarhan <hi@memtarhan.com>
2024-12-14 00:19:54 +0300

    Create LittleLemon project

---
+++ LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Mehmet Tarhan on 14.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
